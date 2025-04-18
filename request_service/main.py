18043942f0357f112de0c408a968b5c0fdf0ea0c
Mat <mathewdenison@gmail.com>
2025-04-17 21:11:15 -0400

    Updates

--- request_service/main.py
+++ request_service/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from shared.sqs_client import send_message
 import os
+from fastapi import Request
+
 
 app = FastAPI()
 
@@ -11,7 +13,11 @@ class UserRequest(BaseModel):
 
 QUEUE_URL = os.getenv("REQUEST_QUEUE_URL")
 
-@app.post("/request/submit")
+@app.post("/submit")
 def submit_request(user_request: UserRequest):
     send_message(QUEUE_URL, user_request.text)
     return {"status": "submitted"}
+
+@app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def catch_all(full_path: str, request: Request):
+    return {"path": full_path, "method": request.method}
